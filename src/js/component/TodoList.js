94737bdc5e0b3aeed039fbc17f99d4ecb92ef989
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-25 23:38:16 +0000

    todolist

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,5 +1,8 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-
-export function TodoList(){
+export function TodoList(props){
+    
+    return(<ul></ul>)
 }
