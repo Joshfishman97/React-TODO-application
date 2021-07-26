a3e845453a8d3a5281e8fad1c59e9ff4e84a2e0c
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-26 01:03:13 +0000

    a

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -2,7 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-export function TodoList(props){
-    
-    return(<ul></ul>)
+export function TodoList(e) {
+	state = {
+		value: ""
+	};
+	return (
+		<div className="headerAndTable">
+			<ul>
+				<h1>todos</h1>
+				<input
+					onKeyDown={e => this.setState({ value: e.target.value })}
+					value={this.state.value}
+				/>
+				value = {this.state.value}
+			</ul>
+
+			{this.state.value}
+		</div>
+	);
 }
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,8 @@
 import React from "react";
-
+import { TodoList } from "./TodoList";
 //include images into your bundle
 
 //create your first component
 export function Home() {
-	return (
-		<TodoList />
-	);
+	return <TodoList />;
 }
