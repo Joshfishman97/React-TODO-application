dd1f851560282403dd127c62b55cc44ace05198b
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-26 21:46:54 +0000

    commit

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-export function TodoList(e) {
-	state = {
-		value: ""
-	};
+export function TodoList() {
+	const [todos, setTodos] = useState(["your todos", "Eat", "Sleep", "Drink"]);
+	const [input, setInput] = useState("");
 	return (
-		<div className="headerAndTable">
-			<ul>
-				<h1>todos</h1>
-				<input
-					onKeyDown={e => this.setState({ value: e.target.value })}
-					value={this.state.value}
-				/>
-				value = {this.state.value}
+		<div className="headerAndTable text-center">
+			<h1>todos</h1>
+			<input
+				onChange={e => setInput(e.target.value)}
+				onKeyUp={e => {
+					if (e.keyCode === 13 && input !== "")
+						setTodos([...todos, input]);
+				}}
+			/>
+			<ul className="list-group">
+				{todos.map((item, index, arr) => {
+					return (
+						<li className="list-group-item" key={index}>
+							{item}
+						</li>
+					);
+				})}
+				{`${todos.length} items left`}
 			</ul>
-
-			{this.state.value}
 		</div>
 	);
 }
