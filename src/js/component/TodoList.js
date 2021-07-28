f75c149c93db99af5cdae35f186e22074feee663
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-28 01:05:59 +0000

    another commit

--- src/js/component/TodoList.js
+++ src/js/component/TodoList.js
@@ -9,18 +9,25 @@ export function TodoList() {
 		<div className="headerAndTable text-center">
 			<h1 className="text-secondary">todos</h1>
 			<input
+				value={input}
 				onChange={e => setInput(e.target.value)}
 				onKeyUp={e => {
-					if (e.keyCode === 13 && input !== "")
+					if (e.keyCode === 13 && input !== "") {
 						setTodos([...todos, input]);
+						setInput("");
+					}
 				}}
 			/>
 			<ul className="list-group">
-				{todos.map((item, index, arr) => {
+				{todos.map((item, index) => {
 					return (
 						<li className="list-group-item" key={index}>
 							{item}{" "}
-							<button className="float-right">
+							<button
+								onClick={() => {
+									todos.splice(index, 1);
+								}}
+								className="float-right">
 								<i className="fas fa-times"></i>
 							</button>
 						</li>
